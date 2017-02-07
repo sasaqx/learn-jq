d9abf1f1e90fa2ee63bed3cca77ac073fb7d338c
hongqiuxia <hongqiuxia@guazi.com>
2017-02-07 14:16:29 +0800

    日历封装

--- Javascript_practive/js/calc.js
+++ Javascript_practive/js/calc.js
@@ -1,35 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no">
-<meta charset="UTF-8">
-<title>倒计时</title>
-<style>
-*{margin:0;padding:0}
-#dateDiv{width:210px;margin:100px auto; overflow:hidden;border:1px solid #000;}
-#dateDiv .name{height:20px; line-height:20px;}
-#dateDiv .name li{height:20px;width:100%; text-align:center; color:blue;list-style:none; position:relative;}
-#dateDiv .name li .a1{ position:absolute;top:0;left:0;}
-#dateDiv .name li .a2{ position:absolute;top:0;right:0;}
-#dateDiv .week{height:30px; line-height:20px;border-bottom:1px solid #000;}
-#dateDiv .week li{ float:left;width:30px;height:30px; text-align:center; list-style:none;}
-#dateDiv .dateUl{ overflow:hidden; clear:both}
-#dateDiv .dateUl li{float:left;width:30px;height:30px; text-align:center; line-height:30px;list-style:none;}
-#dateDiv .dateUl li.ccc{ color:#ccc;}
-#dateDiv .dateUl li.red{ background:#F90}
-#dateDiv .dateUl li.sun{ color:#f00;}
-
-</style>
-<script>
-window.onload = function(){
+function myDate(id){
+    //创建datediv元素
+    var oId = document.getElementById(id); 
+    var dateDiv = document.createElement('div');
+    dateDiv.className = 'dateDiv';
+    document.body.appendChild(dateDiv);
+    dateDiv.innerHTML = '<ul class="name"><li><span></span><a href="javascript:;" class="a1">上月</a><a href="javascript:;" class="a2">下月</a></li></ul><ul class="week"><li>一</li><li>二</li><li>三</li><li>四</li><li>五</li><li style="color:#f00;">六</li><li style="color:#f00;">日</li></ul><ul class="dateUl" id ="dateUl"></ul>';
     //获取相关元素
-    var dateDiv = document.getElementById('dateDiv');
     var oSpan = dateDiv.getElementsByTagName('span')[0];
     var aBtn = dateDiv.getElementsByTagName('a');
-    var oUl = document.getElementById('dateUl');
+    var oUl = dateDiv.children[2];
 
     function createCalc(){
+
         oUl.innerHTML = '';
         //获取第一天星期几，前面空li个数
         var firstDay = getFirstDay(iNow);
@@ -46,6 +28,19 @@ window.onload = function(){
             var aLi = document.createElement('li');
             aLi.innerHTML = i + 1;
             oUl.appendChild(aLi);
+            //每个都要加点击，将当前日期拼入文本框
+
+            aLi.onclick = function(){
+                if (this.className == 'ccc') {
+                    alert('日期过期');
+                }
+                else{
+                    oId.value = oDate.getFullYear()+'-'+(oDate.getMonth()+1)+'-'+ this.innerHTML;
+                    //添加完毕之后隐藏日历
+                    dateDiv.style.display = 'none';
+                }
+            }
+
         }
 
         //设置当前时间 today，判断日期加颜色
@@ -53,6 +48,7 @@ window.onload = function(){
         oDate.setMonth(oDate.getMonth()+iNow);
         var today = oDate.getDate();
         var aLi = oUl.children;
+
         if(iNow < 0){
             for (var i = 0; i< aLi.length;i++){
                 aLi[i].className = 'ccc';
@@ -83,6 +79,7 @@ window.onload = function(){
     }
     //end function
     var iNow = 0;//当月
+    createCalc();
     aBtn[0].onclick = function (){
         iNow--;
         createCalc();
@@ -91,15 +88,19 @@ window.onload = function(){
         iNow++;
         createCalc();
     }
-    createCalc();
-
+    dateDiv.style.left = getPos(oId).left + 'px';
+    dateDiv.style.top = getPos(oId).top + oId.offsetHeight + 'px';
+    dateDiv.style.display = 'none';
+    oId.onclick = function(){
+        dateDiv.style.display = 'block';
+    }
 }
-
+       
 
 //获取当月总天数
 function getDays(iNow){
     var oDate = new Date();
-    oDate.setMonth(oDate.getMonth()+iNow +1);
+    oDate.setMonth(oDate.getMonth()+iNow+1);
     oDate.setDate(0);
     return oDate.getDate();
 }
@@ -112,34 +113,15 @@ function getFirstDay(iNow){
     return oDate.getDay();
 }
 
+// 返回日历当前位置
+function getPos(obj){
+    var l = 0;
+    var t = 0;
+    while (obj) {
+        l+= obj.offsetLeft;
+        t+= obj.offsetTop;
+        obj = obj.offsetParent;
+    }
+    return {left:l,top:t};
+}
 
-
-</script>
-</head>
-
-<body>
-        
-    <div id="dateDiv">
-        <ul class="name">
-            <li>
-               <span></span>
-               <a href="javascript:;" class="a1">上月</a>
-               <a href="javascript:;" class="a2">下月</a>
-            </li>
-        </ul>
-        <ul class="week">
-            <li>一</li>
-            <li>二</li>
-            <li>三</li>
-            <li>四</li>
-            <li>五</li>
-            <li style="color:#f00;">六</li>
-            <li style="color:#f00;">日</li>
-        </ul>
-        <ul class="dateUl" id ="dateUl">
-            
-        </ul>
-    </div>
-    </body>
-
-</html>
